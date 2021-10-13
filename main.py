c972f684515d172994b9bfb45920072d1eb9dd9a
Hakan Tetik <hakantetik@gmail.com>
2021-10-13 11:50:53 +0300

    initiate

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas
+import turtle
+
+screen = turtle.Screen()
+screen.setup(width=1409, height=749)
+image = "harita.png"
+screen.addshape(image)
+turtle.shape(image)
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
